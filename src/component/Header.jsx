f5b738cdc21b6e561f47b1edb0d513f8d0ace841
Naeim_0 <abdelrhmannaeim4@gmail.com>
2023-07-11 02:06:25 +0300

    first commit

---
+++ src/component/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <p>Hey, I am Header Component.</p>
+      </header>
+    </>
+  );
+}
---
+++ src/component/Nav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <>
+      <nav>
+        <h3>Hey, I am Nav Component.</h3>
+      </nav>
+    </>
+  );
+}
